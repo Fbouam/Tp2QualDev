1d26c916f794ad1ab077734415d4fd69ef7312de
Fahed Bouamrani <fahed.bouamrani@etu.univ-orleans.fr>
2024-01-30 16:25:09 +0100

    Question 1 : création de la class Auteur

---
+++ Auteur.java
@@ -0,0 +1,22 @@
+public class Auteur {
+    private String nomAuteur;
+    private String perfTrag;
+    private int qualTrag;
+    private String perfCom;
+    private int qualCom;
+    private String perfDram;
+    private int qualDram;
+
+    public Auteur(String nomAuteur, String perfTrag, int qualTrag, String perfCom, int qualCom, String perfDram, int qualDram) {
+        this.nom = nomAuteur;
+        this.perfTrag = perfTrag;
+        this.qualTrag = qualTrag;
+        this.perfCom = perfCom;
+        this.qualCom = qualCom;
+        this.perfDram = perfDram;
+        this.qualDram = qualDram;
+}
+    public void getQualiteTrag() {
+        return this.qualTrag
+}
+}
